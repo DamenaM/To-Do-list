2a9eacc91944b49c75adb9ff6e8bdb55c6ef24fc
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-25 18:35:20 +0300

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-unresolved
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  devServer: {
+    historyApiFallback: true,
+  },
+  mode: 'production',
 };
