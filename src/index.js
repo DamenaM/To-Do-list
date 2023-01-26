3a5f6e327bb7304d30fcf2f6b88cf31153aa6130
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-26 10:32:18 +0300

    adding list structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const listContainer = document.querySelector('#todo-list');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const LIST = [
+  {
+    description: 'Wash the dish',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete to app list project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'To do project',
+    completed: false,
+    index: 2,
+  },
 
-  return element;
-}
+];
 
-document.body.appendChild(component());
+const loadTask = () => {
+  const sortedList = LIST.sort((item1, item2) => item1.index - item2.index);
+
+  for (let i = 0; i < sortedList.length; i += 1) {
+    listContainer.innerHTML += `
+      <div class="div-container"> 
+        <form class="form-desc">
+           
+         <input type="checkbox" class="checkbox" <p class="description-task">&nbsp&nbsp${sortedList[i].description}
+         <i class="fas fa-ellipsis-v pointer edit-task" aria-hidden="true"></i>
+        </form>
+      </div>
+    `;
+  }
+};
+
+document.addEventListener('DOMContentLoaded', loadTask);
