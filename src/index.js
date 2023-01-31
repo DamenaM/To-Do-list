1a24b594046180c5cec6ced424731471cf25b564
MIhertab Damena <107020397+DamenaM@users.noreply.github.com>
2023-01-31 13:44:37 +0300

    add index

--- src/index.js
+++ src/index.js
@@ -1,41 +1,103 @@
 // import _ from 'lodash';
 import './style.css';
 
-const listContainer = document.querySelector('#todo-list');
-
-const LIST = [
-  {
-    description: 'Wash the dish',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Complete to app list project',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'To do project',
-    completed: false,
-    index: 2,
-  },
-
-];
-
-const loadTask = () => {
-  const sortedList = LIST.sort((item1, item2) => item1.index - item2.index);
-
-  for (let i = 0; i < sortedList.length; i += 1) {
-    listContainer.innerHTML += `
-      <div class="div-container"> 
-        <form class="form-desc">
-           
-         <input type="checkbox" class="checkbox" <p class="description-task">&nbsp&nbsp${sortedList[i].description}
-         <i class="fas fa-ellipsis-v pointer edit-task" aria-hidden="true"></i>
-        </form>
-      </div>
-    `;
+class Task {
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    this.taskTitle = document.querySelector('#task-description');
+    this.taskStatus = document.querySelector('#author');
+    this.taskDisplay = document.querySelector('#display');
+    this.buttonAdd = document.querySelector('#add');
+    this.clearall = document.querySelector('#clearall');
+    this.checkbox = document.querySelector('#checkbox');
+    this.render();
   }
-};
 
-document.addEventListener('DOMContentLoaded', loadTask);
+  addTask(title, status) {
+    this.tasks.push({ title, status });
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.render();
+  }
+
+  removeTask(index) {
+    this.tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.render();
+  }
+
+  addTaskUpdate(index) {
+    this.tasks.splice(index, 0);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.render();
+  }
+
+  removeTaskAll() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.render();
+    document.querySelector('#clearall').forEach((button) => {
+      button.addEventListener('click', () => {
+        this.render();
+      });
+    });
+  }
+
+  /*   disable() {
+    document.getElementById('checkbox').disabled = true;
+  }
+
+  undisable() {
+    document.getElementById('checkbox').disabled = false;
+  } */
+
+  render() {
+    this.taskDisplay.innerHTML = '';
+    this.taskDisplay.classList.add('task_container');
+    if (this.tasks.length === 0) {
+      this.taskDisplay.classList.remove('task_container');
+    }
+    this.tasks.forEach((task, index) => {
+      const div = document.createElement('div');
+      div.innerHTML = `
+      <input type="checkbox" class="checkbox" id="checkbox" >  &nbsp ${task.title} &nbsp
+                <button class="remove-button" data-index="${index}">Remove</button>
+                <br><br>
+            `;
+      if (index % 2 === 0) {
+        div.style.background = '#dddddd';
+      } else {
+        div.style.background = '#fff';
+      }
+
+      this.taskDisplay.appendChild(div);
+      div.classList.add('book_list_container');
+    });
+
+    document.querySelectorAll('.remove-button').forEach((button) => {
+      button.addEventListener('click', () => {
+        this.removeTask(button.getAttribute('data-index'));
+      });
+    });
+
+    document.querySelectorAll('.checkbox').forEach((checkbox) => {
+      checkbox.addEventListener('click', () => {
+        this.addTaskUpdate();
+      });
+    });
+  }
+}
+
+const task = new Task();
+
+task.buttonAdd.addEventListener('click', () => {
+  const title = task.taskTitle.value;
+  const status = 'false';
+
+  task.addTask(title, status);
+  task.taskTitle.value = '';
+  task.taskStatus.value = '';
+});
+
+task.clearall.addEventListener('click', () => {
+  localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  this.remove();
+});
